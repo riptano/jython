ac6019208338f88bb65793d157f70f2494a3e37f
Tom Alexander <tom.alexander@datastax.com>
2016-05-18 12:20:21 -0400

    Fix capath logic error and actually wait for handshakes to complete so we can know if they failed

--- Lib/ssl.py
+++ Lib/ssl.py
@@ -688,9 +688,9 @@ class SSLSocket(object):
             pass
             # see
             # http://stackoverflow.com/questions/24628271/exception-in-netty-io-netty-util-concurrent-blockingoperationexception
-            # - handshake in the child thread pool
-        else:
-            self._sock._handle_channel_future(self._handshake_future, "SSL handshake")
+            # - we are doing this in the handler thread!
+            return
+        self._sock._handle_channel_future(handshake, "SSL handshake", wait=True)
 
     def dup(self):
         raise NotImplementedError("Can't dup() %s instances" %
